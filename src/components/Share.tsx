a3f42b91882e42c107fa0769588c96f98e8467b4
Md Yusuf <mdyusuf742171@gmail.com>
2025-02-07 11:25:21 +0530

    add imagekit integration for media sharing in Share component

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -1,28 +1,100 @@
-import React from 'react'
-import Image from './Image'
+"use client";
+
+import React, { useState } from "react";
+import Image from "./Image";
+import { shareAction } from "@/actions";
+import NextImage from "next/image";
 
 const Share = () => {
+  const [media, setMedia] = useState<File | null>(null);
+
+  const handleMediaChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setMedia(e.target.files[0]);
+    }
+  };
+
+  const previewURL = media ? URL.createObjectURL(media) : null
+
   return (
-    <div className='p-4 flex gap-4'>
-      <div className='relative w-10 h-10 rounded-full overflow-hidden'>
-        <Image path='general/avatar.png' alt='' w={100} h={100} tr={true}/>
+    <form className="p-4 flex gap-4" action={shareAction}>
+      <div className="relative w-10 h-10 rounded-full overflow-hidden">
+        <Image path="general/avatar.png" alt="" w={100} h={100} tr={true} />
       </div>
-      <div className='flex-1 flex flex-col gap-4'>
-        <input type='text' placeholder='what is happening' className='bg-transparent outline-none placeholder:text-textGray text-xl'/>
-        <div className='flex items-center justify-between gap-4 flex-wrap'>
-          <div className='flex gap-4 flex-wrap'>
-            <Image path='icons/image.svg' alt='' w={20} h={20} className='cursor-pointer'/>
-            <Image path='icons/gif.svg' alt='' w={20} h={20} className='cursor-pointer'/>
-            <Image path='icons/poll.svg' alt='' w={20} h={20} className='cursor-pointer'/>
-            <Image path='icons/emoji.svg' alt='' w={20} h={20} className='cursor-pointer'/>
-            <Image path='icons/schedule.svg' alt='' w={20} h={20} className='cursor-pointer'/>
-            <Image path='icons/location.svg' alt='' w={20} h={20} className='cursor-pointer'/>
+      <div className="flex-1 flex flex-col gap-4">
+        <input
+          type="text"
+          name="desc"
+          placeholder="what is happening"
+          className="bg-transparent outline-none placeholder:text-textGray text-xl"
+        />
+        {
+          previewURL && <div className="relative rounded-xl overflow-hidden">
+            <NextImage src={previewURL} alt="" width={600} height={600}/>
+            <div className="absolute top-2 left-2 bg-black bg-opacity-50 text-white py-1 px-4 rounded-full font-bold text-sm cursor-pointer">Edit</div>
           </div>
-          <button className='bg-white text-black font-bold rounded-full py-2 px-4'>Post</button>
+        }
+        <div className="flex items-center justify-between gap-4 flex-wrap">
+          <div className="flex gap-4 flex-wrap">
+            <input
+              type="file"
+              name="file"
+              onChange={handleMediaChange}
+              className="hidden"
+              id="file"
+            />
+            <label htmlFor="file">
+              <Image
+                path="icons/image.svg"
+                alt=""
+                w={20}
+                h={20}
+                className="cursor-pointer"
+              />
+            </label>
+            <Image
+              path="icons/gif.svg"
+              alt=""
+              w={20}
+              h={20}
+              className="cursor-pointer"
+            />
+            <Image
+              path="icons/poll.svg"
+              alt=""
+              w={20}
+              h={20}
+              className="cursor-pointer"
+            />
+            <Image
+              path="icons/emoji.svg"
+              alt=""
+              w={20}
+              h={20}
+              className="cursor-pointer"
+            />
+            <Image
+              path="icons/schedule.svg"
+              alt=""
+              w={20}
+              h={20}
+              className="cursor-pointer"
+            />
+            <Image
+              path="icons/location.svg"
+              alt=""
+              w={20}
+              h={20}
+              className="cursor-pointer"
+            />
+          </div>
+          <button className="bg-white text-black font-bold rounded-full py-2 px-4">
+            Post
+          </button>
         </div>
       </div>
-    </div>
-  )
-}
+    </form>
+  );
+};
 
-export default Share
+export default Share;
