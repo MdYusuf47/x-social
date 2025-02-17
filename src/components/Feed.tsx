e5aabeaef3036b40b4a40faaae2891863086abaa
Md Yusuf <mdyusuf742171@gmail.com>
2025-02-18 00:16:59 +0530

    fetch posts data

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,9 +1,31 @@
 import React from "react";
 import Post from "./Post";
 import { prisma } from "@/prisma";
+import { auth } from "@clerk/nextjs/server";
 
-const Feed = async () => {
-  const posts = await prisma.post.findMany();
+const Feed = async ({ userProfileId }: { userProfileId?: string }) => {
+  const { userId } = await auth();
+  if (!userId) return;
+
+  const whereCondition = userProfileId
+    ? { parentPostId: null, userId: userProfileId }
+    : {
+        parentPostId: null,
+        userId: {
+          in: [
+            userId,
+            ...(
+              await prisma.follow.findMany({
+                where: { followingId: userId },
+                select: { followingId: true },
+              })
+            ).map((follow) => follow.followingId),
+          ],
+        },
+      };
+
+  const posts = await prisma.post.findMany({ where: whereCondition });
+  console.log(posts);
   return (
     <div>
       {posts.map((post) => (
